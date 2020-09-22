68b05bc4b624a7454c9271bff46958cdc32649d3
Evelyn Mendes <evelyn.mendes@gmail.com>
2020-09-21 23:59:04 -0300

    Finalizando controllers

--- src/main/java/com/evelynmendes/bookstoremanager/controller/BookController.java
+++ src/main/java/com/evelynmendes/bookstoremanager/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.evelynmendes.bookstoremanager.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.evelynmendes.bookstoremanager.dto.BookDTO;
 import com.evelynmendes.bookstoremanager.dto.MessageResponseDTO;
+import com.evelynmendes.bookstoremanager.exception.BookNotFoundException;
 import com.evelynmendes.bookstoremanager.service.BookService;
 
 @RestController
@@ -24,6 +27,11 @@ public class BookController {
 	@PostMapping
 	public MessageResponseDTO create(@RequestBody BookDTO bookDTO) {
 		return bookService.create(bookDTO);
+	}
+	
+	@GetMapping("/{id}")
+	public BookDTO findById(@PathVariable Long id) throws BookNotFoundException {
+		return bookService.findById(id);
 	}
 	
 }
--- src/main/java/com/evelynmendes/bookstoremanager/service/BookService.java
+++ src/main/java/com/evelynmendes/bookstoremanager/service/BookService.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Service;
 import com.evelynmendes.bookstoremanager.dto.BookDTO;
 import com.evelynmendes.bookstoremanager.dto.MessageResponseDTO;
 import com.evelynmendes.bookstoremanager.entity.Book;
+import com.evelynmendes.bookstoremanager.exception.BookNotFoundException;
 import com.evelynmendes.bookstoremanager.mapper.BookMapper;
 import com.evelynmendes.bookstoremanager.repository.BookRepository;
+import com.sun.el.stream.Optional ;
 
 @Service
 public class BookService {
@@ -29,6 +31,14 @@ public class BookService {
 		return MessageResponseDTO.builder()
 				.message("Book created with ID " + savedBook.getId())
 				.build();
+	}
+
+	public BookDTO findById(Long id) throws BookNotFoundException {
+		Book book = bookRepository.findById(id)
+				.orElseThrow(() -> new BookNotFoundException(id));
+		
+		return BookMapper.toDTO(book);
+		
 	}
 
 }
